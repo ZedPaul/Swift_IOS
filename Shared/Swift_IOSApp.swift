2d10a2ab67e3a70e757a82788fd9fbdec4c64bb2
ZedPaul <paulkevintornado@gmail.com>
2021-05-12 18:28:17 +0700

    Initial Commit

---
+++ Shared/Swift_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_IOSApp.swift
+//  Shared
+//
+//  Created by Luan Vo Thanh on 5/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
